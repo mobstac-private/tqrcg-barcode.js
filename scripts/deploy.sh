77b5b4b7631e15f524d3160d722008cf03329092
Manu <thembrown@gmail.com>
2021-04-11 12:50:37 +0200

    chore: re-publish to npm

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,20 +9,19 @@ VERSION=$(readJsonProp "package.json" "version")
 echo Deploying version $VERSION...
 
 rm -rf tmp || exit 1;
-git clone "https://${GH_TOKEN}@${GH_BOWER}" tmp
+mkdir tmp
 cp dist/* package.json bower.json LICENSE NOTICE tmp/
 cd tmp
 
-git config user.name "Travis CI"
-git config user.email "mb@w69b.com"
-
-git add -A
-git commit -m "v$VERSION" || true;
-git tag v$VERSION
-
-git push --quiet origin master > /dev/null 2>&1
-git push --quiet origin v$VERSION > /dev/null 2>&1
+# git config user.name "Travis CI"
+# git config user.email "mb@w69b.com"
+# 
+# git add -A
+# git commit -m "v$VERSION" || true;
+# git tag v$VERSION
+# 
+# git push --quiet origin master > /dev/null 2>&1
+# git push --quiet origin v$VERSION > /dev/null 2>&1
 
 echo "Depoying to npm..."
-echo "//registry.npmjs.org/:_authToken=${NPM_API_TOKEN}" > ~/.npmrc
 npm publish
